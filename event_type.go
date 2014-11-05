64545c5e4c2fc29cb3e96d12bc823594cfe8d65e
Robert Mulley <rmulley@fastmail.fm>
2014-11-04 21:00:27 -0500

    Fixed Event_t metadata type, #1

--- event_type.go
+++ event_type.go
@@ -1,9 +1,9 @@
 package intercom
 
 type Event_t struct {
-	EventName string            `json:"event_name"`
-	CreatedAt int64             `json:"created_at"`
-	UserId    uint              `json:"user_id,omitempty"`
-	Email     string            `json:"email,omitempty"`
-	Metadata  map[string]string `json:"metadata,omitempty"`
+	EventName string                 `json:"event_name"`
+	CreatedAt int64                  `json:"created_at"`
+	UserId    uint                   `json:"user_id,omitempty"`
+	Email     string                 `json:"email,omitempty"`
+	Metadata  map[string]interface{} `json:"metadata,omitempty"`
 } //Event_t
