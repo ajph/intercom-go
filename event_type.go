8b2558a1d3e3f24ee110374de4e06c74931d678b
Robert Mulley <rmulley@gmail.com>
2015-01-08 13:00:06 -0500

    Fixed event type and README doc. closes #6

--- event_type.go
+++ event_type.go
@@ -3,7 +3,7 @@ package intercom
 type Event_t struct {
 	EventName string                 `json:"event_name"`
 	CreatedAt int64                  `json:"created_at"`
-	UserId    uint                   `json:"user_id,omitempty"`
+	UserId    string                 `json:"user_id,omitempty"`
 	Email     string                 `json:"email,omitempty"`
 	Metadata  map[string]interface{} `json:"metadata,omitempty"`
 } //Event_t
